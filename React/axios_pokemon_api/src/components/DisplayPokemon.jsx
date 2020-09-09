d4ae30c12a764289840aba530df1af2cc39830ec
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-09-09 11:23:40 -0700

    Complete Axios Pokemon API

---
+++ React/axios_pokemon_api/src/components/DisplayPokemon.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react';
+import ShowPokemon from './ShowPokemon';
+
+const DisplayPokemon = ({pokemon}) => {
+
+    return(
+        <div>
+            {
+                pokemon.map((val, idx) =>
+                    <ShowPokemon key={idx} thisPokemon={val} />
+                )
+            }
+        </div>
+    )
+}
+
+export default DisplayPokemon;
---
+++ React/axios_pokemon_api/src/components/ShowPokemon.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from'react';
+
+const ShowPokemon = ({idx, thisPokemon}) => {
+
+
+    return(
+        <div>
+            Name: {thisPokemon.name}
+        </div>
+    )
+}
+
+export default ShowPokemon;
