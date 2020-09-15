72f2c9e02242f9c9eb031d267017435d1f0254a7
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-09-15 13:17:16 -0700

    Complete Product Manager part 2

--- FullStackMern/product_manager/client/src/views/Main.js
+++ FullStackMern/product_manager/client/src/views/Main.js
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import ProductForm from '../components/ProductForm';
+import ProductList from '../components/ProductList';
+import axios from 'axios';
 
 
 export default () => {
+
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/products')
+            .then(res=> {
+                console.log(res.data.product);
+                setProducts(res.data.product);
+                setLoaded(true);
+            });
+    }, [])
+
     return(
-        <ProductForm/>
+        <div>
+            <ProductForm />
+            <hr />
+            {loaded && <ProductList products={products}/>}
+        </div>
     )
 }
